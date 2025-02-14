1bd0f0b57e95dc9c50cc942ae9460bad514749b8
我心里危险的东西 <iota9star@foxmail.com>
2025-02-14 19:54:43 +0800

    Update web.sh

--- scripts/web.sh
+++ scripts/web.sh
@@ -2,5 +2,5 @@
 
 git clone --depth 1 -b stable https://github.com/flutter/flutter.git flutter-sdk
 export PATH="$PATH:`pwd`/flutter-sdk/bin"
-flutter build web --release --web-renderer html
+flutter build web --release
 dart scripts/web.dart
